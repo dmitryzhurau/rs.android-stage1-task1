5b14fb0c2dbfe1a875583fc53949e2c922e3819b
dmitryzhurau <dmitry.zhurau@gmail.com>
2020-03-29 21:13:53 +0300

    Add Task 1
    Add Task 4
    Add Task 5

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,16 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val happyList = sadArray.toMutableList()
+        var i = 1
+        while (i < happyList.size - 1) {
+            if ( i != 0 && happyList[i] > happyList[i + 1] + happyList[i - 1]) {
+                happyList.removeAt(i)
+                i --
+            } else {
+                i ++
+            }
+        }
+        return happyList.toIntArray()
     }
 }
--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,31 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val str = StringBuilder(digitString)
+        var count = 0
+        for (i in 0 until digitString.length / 2)
+            if (digitString[i] != digitString[digitString.length - i - 1]) count++
+        var m = k - count
+        val result = if (m >= 0) {
+            for (i in 0..digitString.length / 2) {
+                if (m > 1 && str[i] != '9') {
+                    if (str[i] != '9') {
+                        if (str[i] == str[str.length - i - 1]) m -= 2 else m--
+                        str[i] = '9'
+                        str[str.length - i - 1] = '9'
+                    }
+                } else if (str[i] != str[str.length - i - 1]) {
+                    if (m == 1 && str[i] != '9') {
+                        str[i] = '9'
+                        str[str.length - i - 1] = '9'
+                        m--
+                    } else if (str[i] > str[str.length - i - 1]) str[str.length - i - 1] = str[i]
+                    else str[i] = str[str.length - i - 1]
+                }
+            }
+            if (str.length % 2 == 1 && m > 0) str[(str.length - 1) / 2] = '9'
+            str.toString()
+        } else "-1"
+        return result
     }
 }
